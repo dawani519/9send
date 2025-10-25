ce595beb6afc3fffa463b342560de3fd7ca2150f
dawani519 <danielamos519@gmail.com>
2025-10-26 00:33:59 +0100

    updated config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 # Twilio
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_WH_SID = os.getenv('TWILIO_WHATSAPP_NUMBER', 'whatsapp:+14155238886')
+TWILIO_WH_SID = os.getenv('TWILIO_WHATSAPP_NUMBER', 'whatsapp:+19788008705')
 
 # Flutterwave (Test Mode)
 FLUTTERWAVE_SECRET_KEY = os.getenv('FLUTTERWAVE_SECRET_KEY', 'FLWSECK_TEST-XXXXXXXXXXXXXXXXXXXXXXXXX')
@@ -18,8 +18,7 @@ SUMSUB_API_KEY = os.getenv('SUMSUB_API_KEY', 'test_key_123')
 SUMSUB_BASE_URL = 'https://test-api.sumsub.com'
 
 # Chipper Cash (Sandbox)
-CHIPPER_API_KEY = os.getenv('CHIPPER_API_KEY', 'test_key_456')
-CHIPPER_BASE_URL = 'https://api-sandbox.chippercash.com/v1'
+
 
 # Database
 DATABASE_URL = os.getenv('DATABASE_URL')  # Render auto-provides

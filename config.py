fdaef8ac3af49144b4a2be738a5425c7bcb82258
dawani519 <danielamos519@gmail.com>
2025-10-26 00:35:48 +0100

    updated config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ FLUTTERWAVE_SECRET_KEY = os.getenv('FLUTTERWAVE_SECRET_KEY', 'FLWSECK_TEST-XXXXX
 FLUTTERWAVE_PUBLIC_KEY = os.getenv('FLUTTERWAVE_PUBLIC_KEY', 'FLWPUBK_TEST-XXXXXXXXXXXXXXXXXXXXXXXXX')
 
 # SumSub (Free Trial)
-SUMSUB_API_KEY = os.getenv('SUMSUB_API_KEY', 'test_key_123')
-SUMSUB_BASE_URL = 'https://test-api.sumsub.com'
+#SUMSUB_API_KEY = os.getenv('SUMSUB_API_KEY', 'test_key_123')
+#SUMSUB_BASE_URL = 'https://test-api.sumsub.com'
 
 # Chipper Cash (Sandbox)
 
